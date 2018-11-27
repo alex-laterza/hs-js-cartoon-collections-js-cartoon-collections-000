f3180687d75688da1540fcb50f27af85ee2ca2bf
Alessandro Laterza <alecs.laterza@gmail.com>
2018-11-27 00:52:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function dwarfRollCall(dwarves) {
+  var nani = "";
+  dwarves.forEach(function(name, idx) {
+  	nani += (idx+1 + ". " + name + " ");
+  });
+  return nani;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  
 }
 
 function longPlaneteerCalls(words) {
